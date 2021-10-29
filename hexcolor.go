9c64128c5fa01dab94f360c00e89af95a794a4f4
rgoupil <goupil.robin@gmail.com>
2021-10-29 15:22:05 +0200

    feat: alpha component (#7)
    Added alpha component support, added tests and benchmarks.

--- hexcolor.go
+++ hexcolor.go
@@ -31,10 +31,20 @@ func Parse(hex string) (c color.RGBA, err error) {
 	}
 
 	switch len(hex) {
+	case 9:
+		c.R = (hexToByte(hex[1]) << 4) + hexToByte(hex[2])
+		c.G = (hexToByte(hex[3]) << 4) + hexToByte(hex[4])
+		c.B = (hexToByte(hex[5]) << 4) + hexToByte(hex[6])
+		c.A = (hexToByte(hex[7]) << 4) + hexToByte(hex[8])
 	case 7:
 		c.R = (hexToByte(hex[1]) << 4) + hexToByte(hex[2])
 		c.G = (hexToByte(hex[3]) << 4) + hexToByte(hex[4])
 		c.B = (hexToByte(hex[5]) << 4) + hexToByte(hex[6])
+	case 5:
+		c.R = hexToByte(hex[1]) * 17
+		c.G = hexToByte(hex[2]) * 17
+		c.B = hexToByte(hex[3]) * 17
+		c.A = hexToByte(hex[4]) * 17
 	case 4:
 		c.R = hexToByte(hex[1]) * 17
 		c.G = hexToByte(hex[2]) * 17
